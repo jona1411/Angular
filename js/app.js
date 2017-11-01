45784d493a70a1a7cdb109350fd2c5936f69c931
Balefather <balefather@gmail.com>
2017-11-01 11:48:06 +0100

    add premade recipe functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 var app = angular.module("myShoppingList", []);
 app.controller("myCtrl", function($scope) {
-    $scope.products = ["Milk", "Bread", "Cheese"];
+    $scope.products = [];
     $scope.addItem = function () {
         $scope.products.push($scope.addMe);
     }
     $scope.removeItem = function (x) {
         $scope.products.splice(x,1);
+    }
+    var bolognese = ["Tomato", "Spaghetti"]
+    $scope.addRecipe = function() {
+        $scope.products = $scope.products.concat(bolognese);
     }
 });
