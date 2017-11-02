7121745af771d96be68d00fe1d9c31d79981783e
Balefather <balefather@gmail.com>
2017-11-02 03:07:21 +0100

    Lots of styling
    Hallihallå

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 var app = angular.module("myShoppingList", []);
 app.controller("myCtrl", function($scope) {
-    $scope.products = [];
+    $scope.products = ["Cheese", "Wheat"];
     $scope.addItem = function () {
         $scope.products.push($scope.addMe);
-    }
+    };
+
     $scope.removeItem = function (x) {
         $scope.products.splice(x,1);
-    }
+    };
 
-    var bolognese = ["Tomato", "Spaghetti"]
+    var bolognese = ["Tomato","Spaghetti"];
+
     $scope.addRecipe = function() {
         $scope.products = $scope.products.concat(bolognese);
-    }
+    };
 });
 
